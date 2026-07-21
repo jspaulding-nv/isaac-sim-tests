f167dfb180c6849ed81ebfd576942da447ee1c96
jspaulding-nv <183430844+jspaulding-nv@users.noreply.github.com>
2026-07-21 17:14:50 +0000

    Make privacy audit portable in CI

--- scripts/audit_public_tree.sh
+++ scripts/audit_public_tree.sh
@@ -9,12 +9,26 @@ cd "$REPO_ROOT"
 
 pattern='(/home/[^/[:space:]]+|10\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}|192\.168\.[0-9]{1,3}\.[0-9]{1,3}|172\.(1[6-9]|2[0-9]|3[01])\.[0-9]{1,3}\.[0-9]{1,3}|[[:alnum:]._%+-]+@[[:alnum:].-]+\.[[:alpha:]]{2,}|(token|password|secret)[[:space:]]*[:=][[:space:]]*[^[:space:]]+)'
 
+scan_tree() {
+    local search_pattern="$1"
+
+    if command -v rg >/dev/null 2>&1; then
+        rg -n -i --hidden \
+            --glob '!.git/**' \
+            --glob '!output/**' \
+            --glob '!scripts/audit_public_tree.sh' \
+            "$search_pattern" .
+    else
+        grep -R -n -i -E \
+            --exclude-dir=.git \
+            --exclude-dir=output \
+            --exclude=audit_public_tree.sh \
+            -- "$search_pattern" .
+    fi
+}
+
 set +e
-rg -n -i --hidden \
-    --glob '!.git/**' \
-    --glob '!output/**' \
-    --glob '!scripts/audit_public_tree.sh' \
-    "$pattern" .
+scan_tree "$pattern"
 status=$?
 set -e
 
@@ -26,8 +40,7 @@ if [[ "$status" -gt 1 ]]; then
     exit "$status"
 fi
 
-if [[ -n "${EXTRA_DENY_REGEX:-}" ]] && rg -n -i --hidden \
-    --glob '!.git/**' --glob '!output/**' "$EXTRA_DENY_REGEX" .; then
+if [[ -n "${EXTRA_DENY_REGEX:-}" ]] && scan_tree "$EXTRA_DENY_REGEX"; then
     printf 'A caller-supplied deny term was found.\n' >&2
     exit 1
 fi
